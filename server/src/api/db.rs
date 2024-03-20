6c0cbc8e7c9a3455f1b924734832e1d26cbe71a0
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-03-20 04:59:36 +0300

    Added `insert_message` method to DB

--- server/src/api/db.rs
+++ server/src/api/db.rs
@@ -1,22 +1,33 @@
-use std::future::Future;
+use std::{borrow::Cow, future::Future};
 
-use common::types::{Id, UserId};
+use common::types::{message_id::MessageId, Id, UserId};
 use sqlx::SqlitePool;
 
-/// Trait for polymorphically running queries on different databases
-pub trait DbQuery {
-    /// Insert a new user
-    fn insert_user<'a>(
-        &'a self,
-        id: &'a UserId,
-    ) -> impl Future<Output = sqlx::Result<()>> + Send + '_;
+/// Shortcut for `impl Future<Output = t> + Send`
+macro_rules! send_future {
+    ($t:ty) => {
+        impl Future<Output = $t> + Send
+    };
+    ($t:ty, $l:lifetime) => {
+        impl Future<Output = $t> + Send + $l
+    }
 }
 
-impl DbQuery for SqlitePool {
-    fn insert_user<'a>(
-        &'a self,
-        id: &'a UserId,
-    ) -> impl Future<Output = sqlx::Result<()>> + Send + '_ {
+/// Trait for polymorphically running queries on different databases
+pub trait Db {
+    /// Insert a new user
+    fn insert_user(&self, id: &UserId) -> send_future!(sqlx::Result<()>);
+    fn insert_message(
+        &self,
+        id: &MessageId,
+        from: &UserId,
+        to: &UserId,
+        content: &[u8],
+    ) -> send_future!(sqlx::Result<()>);
+}
+
+impl Db for SqlitePool {
+    fn insert_user(&self, id: &UserId) -> send_future!(sqlx::Result<()>) {
         async move {
             let id_bytes = id.as_bytes();
 
@@ -25,6 +36,36 @@ impl DbQuery for SqlitePool {
                 INSERT INTO users VALUES (?);
                 ",
                 id_bytes,
+            )
+            .execute(self)
+            .await?;
+
+            Ok(())
+        }
+    }
+
+    fn insert_message(
+        &self,
+        id: &MessageId,
+        from: &UserId,
+        to: &UserId,
+        content: &[u8],
+    ) -> send_future!(sqlx::Result<()>) {
+        async move {
+            let id = id.as_bytes();
+            let from = from.as_bytes();
+            let to = to.as_bytes();
+
+            sqlx::query!(
+                "--sql
+                INSERT INTO messages
+                    (id, sender_id, recipient_id, content, is_received)
+                    VALUES (?, ?, ?, ?, FALSE);
+                ",
+                id,
+                from,
+                to,
+                content,
             )
             .execute(self)
             .await?;
