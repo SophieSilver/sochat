e6420ddfebb07b075d7ac32ce68af71bae1a56cb
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-03-23 11:59:41 +0300

    Implemented sending messages in the API.

--- server/src/api.rs
+++ server/src/api.rs
@@ -1,9 +1,25 @@
 pub mod state;
 
-use crate::{db::Db, error::AppResult, extractors::OctetStream};
-use axum::{extract::State, routing, Router};
-use common::types::{Id, UserId};
+use crate::{
+    db::Db,
+    error::{AppError, AppResult},
+    extractors::OctetStream,
+};
+use axum::{
+    body::Bytes,
+    extract::{Path, State},
+    http::StatusCode,
+    routing, Router,
+};
+use common::types::{message_id::MessageId, Id, UserId};
+use serde::Deserialize;
 use state::AppState;
+
+#[derive(Debug, Clone, Copy, Deserialize)]
+struct MessagePathParams {
+    pub user_id: UserId,
+    pub other_id: UserId,
+}
 
 async fn register_user(state: State<AppState>) -> AppResult<OctetStream<UserId>> {
     let new_id = UserId::generate();
@@ -12,12 +28,37 @@ async fn register_user(state: State<AppState>) -> AppResult<OctetStream<UserId>>
     Ok(new_id.into())
 }
 
+async fn send_message(
+    state: State<AppState>,
+    Path(MessagePathParams { user_id, other_id }): Path<MessagePathParams>,
+    content: Bytes,
+) -> AppResult<OctetStream<MessageId>> {
+    let message_id = MessageId::generate();
+
+    let result = state
+        .db()
+        .insert_message(&message_id, &user_id, &other_id, &content)
+        .await;
+
+    match result {
+        Ok(_) => Ok(message_id.into()),
+        Err(sqlx::Error::Database(e)) if e.is_foreign_key_violation() => Err(AppError::new(
+            StatusCode::NOT_FOUND,
+            "User with that ID does not exist",
+        )),
+        Err(_) => Err(AppError::generic()),
+    }
+}
+
 /// Create a router with all API endpoints
 pub fn router(app_state: AppState) -> Router<()> {
     use routing::method_routing as method;
 
-    Router::new().route(
-        "/users",
-        method::post(register_user).with_state(app_state.clone()),
-    )
+    Router::new()
+        .route("/users", method::post(register_user))
+        .route(
+            "/messages/from/:user_id/to/:other_id",
+            method::post(send_message),
+        )
+        .with_state(app_state.clone())
 }
