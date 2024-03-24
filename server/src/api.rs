fd86bd603e41ca937cc75e516d8f523e7c538cc7
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-03-24 11:13:15 +0300

    Implemented endpoints for fetching messages and marking messages received

--- server/src/api.rs
+++ server/src/api.rs
@@ -3,22 +3,36 @@ pub mod state;
 use crate::{
     db::Db,
     error::{AppError, AppResult},
-    extractors::OctetStream,
+    extractors::{Cbor, OctetStream},
 };
 use axum::{
     body::Bytes,
-    extract::{Path, State},
+    extract::{Path, Query, State},
     http::StatusCode,
     routing, Router,
 };
-use common::types::{message_id::MessageId, Id, UserId};
+use common::types::{message_id::MessageId, Id, UnreadMessage, UserId};
 use serde::Deserialize;
+use smallvec::SmallVec;
 use state::AppState;
 
 #[derive(Debug, Clone, Copy, Deserialize)]
 struct MessagePathParams {
-    pub user_id: UserId,
+    /// ID of the user initiating the request
+    ///
+    /// When sending messages it is the sender's ID,
+    /// When receiving messages it is the recipient's ID
+    pub self_id: UserId,
+    /// ID of the other user in the conversation
+    ///
+    /// When sending messages it is the recipient's ID,
+    /// When receiving messages it is the sender's ID
     pub other_id: UserId,
+}
+
+#[derive(Debug, Clone, Copy, Deserialize)]
+struct LimitQueryParam {
+    pub limit: u32,
 }
 
 async fn register_user(state: State<AppState>) -> AppResult<OctetStream<UserId>> {
@@ -30,14 +44,17 @@ async fn register_user(state: State<AppState>) -> AppResult<OctetStream<UserId>>
 
 async fn send_message(
     state: State<AppState>,
-    Path(MessagePathParams { user_id, other_id }): Path<MessagePathParams>,
+    Path(MessagePathParams {
+        self_id: sender_id,
+        other_id: recipient_id,
+    }): Path<MessagePathParams>,
     content: Bytes,
 ) -> AppResult<OctetStream<MessageId>> {
     let message_id = MessageId::generate();
 
     let result = state
         .db()
-        .insert_message(&message_id, &user_id, &other_id, &content)
+        .insert_message(&message_id, &sender_id, &recipient_id, &content)
         .await;
 
     match result {
@@ -50,6 +67,48 @@ async fn send_message(
     }
 }
 
+async fn mark_received(
+    state: State<AppState>,
+    Path(MessagePathParams {
+        other_id: sender_id,
+        self_id: recipient_id,
+    }): Path<MessagePathParams>,
+    Cbor(message_ids): Cbor<SmallVec<[MessageId; 4]>>, // the most common case would only have 1 ID
+) -> AppResult<StatusCode> {
+    // TODO: have a limit and make it configurable
+    state
+        .db()
+        .mark_messages_received(&sender_id, &recipient_id, &message_ids)
+        .await?;
+
+    Ok(StatusCode::NO_CONTENT)
+}
+
+async fn fetch_messages(
+    state: State<AppState>,
+    Path(MessagePathParams {
+        other_id: sender_id,
+        self_id: recipient_id,
+    }): Path<MessagePathParams>,
+    limit: Option<Query<LimitQueryParam>>,
+) -> AppResult<Cbor<Box<[UnreadMessage]>>> {
+    // TODO: make this configurable
+    const DEFAULT_LIMIT: u32 = 10;
+    const MAX_LIMIT: u32 = 32;
+
+    let limit = limit
+        .map(|wrapper| wrapper.0.limit)
+        .unwrap_or(DEFAULT_LIMIT)
+        .min(MAX_LIMIT);
+
+    let messages = state
+        .db()
+        .fetch_unread_messages(&sender_id, &recipient_id, limit)
+        .await?;
+
+    Ok(messages.into())
+}
+
 /// Create a router with all API endpoints
 pub fn router(app_state: AppState) -> Router<()> {
     use routing::method_routing as method;
@@ -57,8 +116,16 @@ pub fn router(app_state: AppState) -> Router<()> {
     Router::new()
         .route("/users", method::post(register_user))
         .route(
-            "/messages/from/:user_id/to/:other_id",
+            "/messages/from/:self_id/to/:other_id",
             method::post(send_message),
+        )
+        .route(
+            "/messages/from/:other_id/to/:self_id/received",
+            method::post(mark_received),
+        )
+        .route(
+            "/messages/from/:other_id/to/:self_id",
+            method::get(fetch_messages),
         )
         .with_state(app_state.clone())
 }
