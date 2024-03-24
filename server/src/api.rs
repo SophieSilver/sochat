6847b1010d6861c493e90a6f7cfb7f9d9c4a414e
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-03-24 12:31:29 +0300

    Changed path parameters to have the same names.
    The previous scheme was causing routes conflicts in axum.

--- server/src/api.rs
+++ server/src/api.rs
@@ -18,16 +18,8 @@ use state::AppState;
 
 #[derive(Debug, Clone, Copy, Deserialize)]
 struct MessagePathParams {
-    /// ID of the user initiating the request
-    ///
-    /// When sending messages it is the sender's ID,
-    /// When receiving messages it is the recipient's ID
-    pub self_id: UserId,
-    /// ID of the other user in the conversation
-    ///
-    /// When sending messages it is the recipient's ID,
-    /// When receiving messages it is the sender's ID
-    pub other_id: UserId,
+    pub sender_id: UserId,
+    pub recipient_id: UserId,
 }
 
 #[derive(Debug, Clone, Copy, Deserialize)]
@@ -45,8 +37,8 @@ async fn register_user(state: State<AppState>) -> AppResult<OctetStream<UserId>>
 async fn send_message(
     state: State<AppState>,
     Path(MessagePathParams {
-        self_id: sender_id,
-        other_id: recipient_id,
+        sender_id,
+        recipient_id,
     }): Path<MessagePathParams>,
     content: Bytes,
 ) -> AppResult<OctetStream<MessageId>> {
@@ -70,8 +62,8 @@ async fn send_message(
 async fn mark_received(
     state: State<AppState>,
     Path(MessagePathParams {
-        other_id: sender_id,
-        self_id: recipient_id,
+        sender_id,
+        recipient_id,
     }): Path<MessagePathParams>,
     Cbor(message_ids): Cbor<SmallVec<[MessageId; 4]>>, // the most common case would only have 1 ID
 ) -> AppResult<StatusCode> {
@@ -87,8 +79,8 @@ async fn mark_received(
 async fn fetch_messages(
     state: State<AppState>,
     Path(MessagePathParams {
-        other_id: sender_id,
-        self_id: recipient_id,
+        sender_id,
+        recipient_id,
     }): Path<MessagePathParams>,
     limit: Option<Query<LimitQueryParam>>,
 ) -> AppResult<Cbor<Box<[UnreadMessage]>>> {
@@ -116,16 +108,16 @@ pub fn router(app_state: AppState) -> Router<()> {
     Router::new()
         .route("/users", method::post(register_user))
         .route(
-            "/messages/from/:self_id/to/:other_id",
+            "/messages/from/:sender_id/to/:recipient_id",
             method::post(send_message),
         )
         .route(
-            "/messages/from/:other_id/to/:self_id/received",
-            method::post(mark_received),
+            "/messages/from/:sender_id/to/:recipient_id",
+            method::get(fetch_messages),
         )
         .route(
-            "/messages/from/:other_id/to/:self_id",
-            method::get(fetch_messages),
+            "/messages/from/:sender_id/to/:recipient_id/received",
+            method::post(mark_received),
         )
         .with_state(app_state.clone())
 }
