062e18d565550c8f3e22c338afb4ebad21ac2254
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-03-26 12:47:39 +0300

    Added tracing.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,32 +1,42 @@
-#![cfg_attr(debug_assertions, allow(unused))]
-
-use std::net::ToSocketAddrs;
-
-use common::utils::CompactUuid;
-use serde::{Deserialize, Serialize};
-use serde_with::serde_as;
+// #![cfg_attr(debug_assertions, allow(unused))]
+use std::{
+    net::SocketAddr,
+    str::FromStr,
+};
 use server::api::{self, state::AppState};
 use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode};
-use uuid::Uuid;
+use tower_http::trace::TraceLayer;
+use tracing::Level;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_target(true)
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let connection_options = SqliteConnectOptions::new()
         .filename("run.db")
         .create_if_missing(true)
         .journal_mode(SqliteJournalMode::Wal);
+
     let db_pool = sqlx::SqlitePool::connect_with(connection_options).await?;
 
     sqlx::migrate!().run(&db_pool).await?;
 
     let app_state = AppState::new(db_pool.clone());
+    let router = api::router(app_state)
+        .layer(tower::ServiceBuilder::new().layer(TraceLayer::new_for_http()));
+
+    let app = router.into_make_service_with_connect_info::<SocketAddr>();
+    let addr = SocketAddr::from_str("127.0.0.1:11800").unwrap();
 
     tokio::select! {
-    _ = axum_server::bind("127.0.0.1:11800".to_socket_addrs().unwrap().next().unwrap())
-        .serve(api::router(app_state).into_make_service()) => (),
-    _ = tokio::signal::ctrl_c() => ()
+        _ = axum_server::bind(addr).serve(app) => (),
+        _ = tokio::signal::ctrl_c() => ()
     };
-    
+
     println!("exiting");
     db_pool.close().await;
 
