b2796bb85698ce63f06296c8adfca9f6588d9d68
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-05-29 00:26:46 +0300

    Client: implemented `textExtraVerticalPadding` util

--- client/lib/utils/padding.dart
+++ client/lib/utils/padding.dart
@@ -9,7 +9,24 @@ import 'package:flutter/material.dart';
 /// 
 /// This function automatically calculates how much extra padding the top and bottom sides will have,
 /// so that it can be subtracted from the subsequent padding calculations
+/// 
+/// # Returns
+/// * Observed extra vertical padding on __one__ side 
+/// * 0.0 if `style.fontSize` or `style.height` are null
+/// 
+/// # Note
+/// This function presumes and even amount of space added at the top and bottom of the text.
+/// That might not always be true. It's recommended to set `textHeightBehavior`'s 
+/// `leadingDistribution` to `TextLeadingDistribution.even`
 double textExtraVerticalPadding(TextStyle style) {
-  // TODO: implement
-  throw UnimplementedError();
+  final fontSize = style.fontSize;
+  final lineHeight = style.height;
+  
+  if (fontSize == null || lineHeight == null) {
+    return 0.0;
+  }
+  
+  final lineHeightPixels = fontSize * lineHeight;
+  
+  return (lineHeightPixels - fontSize) / 2.0;
 }
