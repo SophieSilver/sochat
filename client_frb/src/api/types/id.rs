34407942e585d3d935b2ff9ff3bdd13a1ec98eba
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-06-29 02:09:09 +0300

    Client: removed hashCode implementation from IDs.
    Since equality operators cannot be overriden, correct implementation of hashCode is currently impossible

--- client_frb/src/api/types/id.rs
+++ client_frb/src/api/types/id.rs
@@ -23,17 +23,20 @@ where
 
     #[frb(sync, getter)]
     fn hash_code(&self) -> i64 {
-        const SIZE: usize = mem::size_of::<i64>();
+        unimplemented!("Please avoid hashing IDs. \n\
+Due to FRB limitations equality operator cannot be overriden, therefore a hashCode cannot be defined correctly");
 
-        let bytes = self.as_bytes();
-        let mut hash_code_bytes = [0u8; SIZE];
+        // const SIZE: usize = mem::size_of::<i64>();
 
-        for (index, &byte) in bytes.iter().enumerate() {
-            // XORing with the byte at that location
-            hash_code_bytes[index % SIZE] ^= byte;
-        }
+        // let bytes = self.as_bytes();
+        // let mut hash_code_bytes = [0u8; SIZE];
 
-        i64::from_le_bytes(hash_code_bytes)
+        // for (index, &byte) in bytes.iter().enumerate() {
+        //     // XORing with the byte at that location
+        //     hash_code_bytes[index % SIZE] ^= byte;
+        // }
+
+        // i64::from_le_bytes(hash_code_bytes)
     }
 
     /// Use instead of == operator due to FRB limitations
@@ -46,19 +49,19 @@ where
 macro_rules! extend_id {
     ($type_name: ident, $mirror_name: ident) => {
         #[frb(
-            mirror($type_name),
-        //     dart_code =
-        // "
-        //     @override
-        //     bool operator ==(Object other) {
-        //         if (this.runtimeType != other.runtimeType) {
-        //             return false;
-        //         }
+                    mirror($type_name),
+                //     dart_code =
+                // "
+                //     @override
+                //     bool operator ==(Object other) {
+                //         if (this.runtimeType != other.runtimeType) {
+                //             return false;
+                //         }
 
-        //         return this.equals(other as dynamic);
-        //     }
-        // "
-        )]
+                //         return this.equals(other as dynamic);
+                //     }
+                // "
+                )]
         struct $mirror_name {
             _opq: Opaque,
         }
