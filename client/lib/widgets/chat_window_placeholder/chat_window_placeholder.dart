81c648812e33ac53a605df64577cc12f83e92001
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-06-29 02:38:20 +0300

    Client: added a chat window placeholder

--- client/lib/widgets/chat_window_placeholder/chat_window_placeholder.dart
+++ client/lib/widgets/chat_window_placeholder/chat_window_placeholder.dart
@@ -2,9 +2,28 @@ import 'package:flutter/material.dart';
 
 class ChatWindowPlaceholder extends StatelessWidget {
   const ChatWindowPlaceholder({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    final textStyle = theme.textTheme;
+
+    return Center(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 50.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("No conversations open", style: textStyle.headlineSmall),
+            SizedBox(height: 20.0),
+            Text("Add a new contact and start a conversation", style: textStyle.titleLarge, textAlign: TextAlign.center,),
+            SizedBox(height: 20.0),
+            Icon(Icons.arrow_back, size: 50.0,)
+          ],
+        ),
+      ),
+    );
   }
 }
