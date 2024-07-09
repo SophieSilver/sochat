d61130f92ae04c67c382fc14e51216c299ee1725
Sophie Leochina <87021304+SophieSilver@users.noreply.github.com>
2024-07-09 10:31:48 +0300

    refactor(client_lib): removed `#![forbid(missing_docs)]`

--- client_lib/src/lib.rs
+++ client_lib/src/lib.rs
@@ -1,7 +1,6 @@
 //! Library crate containing core functionality for the SoChat client, such as communicating with the backend servers,
 //! storing user data on disk, and encryption.
 
-#![forbid(missing_docs)]
 pub use common;
 
 pub mod types;
